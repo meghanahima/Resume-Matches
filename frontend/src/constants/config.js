9d7a8d759332b863ed4c89c868048103f67cc52f
meghanahima <meghanasiddi20@gmail.com>
2025-03-22 15:45:05 +0530

    fixed host url logic

--- frontend/src/constants/config.js
+++ frontend/src/constants/config.js
@@ -1,5 +1,5 @@
 export const getCurrentHost = () => {
   // You can add logic here to determine which host to use
   // For now, returning localhost
-  return HOSTS[import.meta.env.VITE_SERVER_URL].hostUrl;
+  return import.meta.env.VITE_SERVER_URL;
 }; 
