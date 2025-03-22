0ea00079d689b2498de4008e47bbb146a8d1ba87
meghanahima <meghanasiddi20@gmail.com>
2025-03-22 15:36:27 +0530

    added server url in env and changed config file in frontend

--- frontend/src/constants/config.js
+++ frontend/src/constants/config.js
@@ -1,16 +1,5 @@
-export const HOSTS = [
-  {
-    name: "localhost",
-    hostUrl: "http://localhost:5000"
-  },
-  {
-    name: "production",
-    hostUrl: "http://alkdfj:5000"
-  }
-];
-
 export const getCurrentHost = () => {
   // You can add logic here to determine which host to use
   // For now, returning localhost
-  return HOSTS[0].hostUrl;
+  return HOSTS[import.meta.env.VITE_SERVER_URL].hostUrl;
 }; 
